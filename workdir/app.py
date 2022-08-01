33599ff6caf70b33569a8fa5e3d3f14070e68ffc
Magne Alvheim <magne@alvheim.com>
2022-08-01 14:14:38 +0200

    Adduser rutiner

--- workdir/app.py
+++ workdir/app.py
@@ -4,6 +4,45 @@ import datetime
 import hashlib
 import jwt
 import json
+import os
+
+def createUserHash(userName, userEmail, userPass):
+    salt = os.urandom(32) # Remember this
+
+    key = hashlib.pbkdf2_hmac(
+        'sha256', # The hash digest algorithm for HMAC
+        userPass.encode('utf-8'), # Convert the password to bytes
+        salt, # Provide the salt
+        100000, # It is recommended to use at least 100,000 iterations of SHA-256 
+        dklen=128 # Get a 128 byte key
+    )
+
+    hash = salt + key 
+
+    item = {
+        'email':      userEmail,
+        'fullname':   userName,
+        'hashedpass': salt + key,
+        'password':   userPass
+    }
+    print(item)
+    return item
+
+def writeUserToDatabase(userRecord):
+    TABLE_NAME = "users"
+
+    # Creating the DynamoDB Client
+    dynamodb_client = boto3.client('dynamodb', region_name="eu-west-1")
+
+    # Creating the DynamoDB Table Resource
+    dynamodb = boto3.resource('dynamodb', region_name="eu-west-1")
+    table = dynamodb.Table(TABLE_NAME)
+
+    item = createUserHash()
+    response = table.put_item(
+        TableName=TABLE_NAME, 
+        Item=item
+    )
 
 def getuser(email):
     table = boto3.resource('dynamodb').Table('users')
